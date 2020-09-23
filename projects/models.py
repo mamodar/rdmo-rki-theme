34636d7498d9956527a7e836a18db561319289d1
Kyanoush Yahosseini <kyanoush.yahosseini@gmail.com>
2020-09-23 12:10:08 +0200

    Replace title by working title

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,3 @@
-from datetime import date, timedelta
-from itertools import zip_longest
-
 import iso8601
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
@@ -8,8 +5,6 @@ from django.db import models
 from django.urls import reverse
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
-
-from rdmo.conditions.models import Condition
 from rdmo.core.constants import (VALUE_TYPE_BOOLEAN, VALUE_TYPE_CHOICES,
                                  VALUE_TYPE_DATETIME, VALUE_TYPE_TEXT)
 from rdmo.core.models import Model
@@ -19,8 +14,8 @@ from rdmo.questions.models import Catalog, Question
 from rdmo.tasks.models import Task
 from rdmo.views.models import View
 
-from .managers import (IssueManager, MembershipManager, ProjectManager,
-                       SnapshotManager, ValueManager)
+from .managers import (MembershipManager, ProjectManager, SnapshotManager,
+                       ValueManager)
 
 
 class Project(Model):
@@ -53,7 +48,7 @@ class Project(Model):
         help_text=_('The catalog which will be used for this project.')
     )
     tasks = models.ManyToManyField(
-        Task, blank=True, through='Issue',
+        Task, blank=True,
         verbose_name=_('Tasks'),
         help_text=_('The tasks that will be used for this project.')
     )
@@ -98,39 +93,6 @@ class Project(Model):
     def guests(self):
         return self.user.filter(membership__role='guest')
 
-    def get_view_conditions(self, snapshot=None):
-        conditions = {}
-        for condition in Condition.objects.all():
-            conditions[condition.key] = condition.resolve(self, snapshot)
-
-        return conditions
-
-    def get_view_values(self, snapshot=None):
-        # get all values for this snapshot and put them in a dict labled by the values attibute path
-        values = {
-            'project/title': [[Value(text=self.title, value_type=VALUE_TYPE_TEXT)]],
-            'project/description': [[Value(text=self.description, value_type=VALUE_TYPE_TEXT)]],
-            'project/created': [[Value(text=self.created, value_type=VALUE_TYPE_DATETIME)]],
-            'project/updated': [[Value(text=self.updated, value_type=VALUE_TYPE_DATETIME)]],
-        }
-
-        for value in self.values.filter(snapshot=snapshot):
-            if value.attribute:
-                attribute_path = value.attribute.path
-                set_index = value.set_index
-
-                # create entry for this values attribute in the values_dict
-                if attribute_path not in values:
-                    values[attribute_path] = []
-
-                # add this value to the values
-                try:
-                    values[attribute_path][set_index].append(value)
-                except IndexError:
-                    values[attribute_path].append([value])
-
-        return values
-
 
 class Membership(models.Model):
 
@@ -169,91 +131,6 @@ class Membership(models.Model):
 
     def get_absolute_url(self):
         return reverse('project', kwargs={'pk': self.project.pk})
-
-
-class Issue(models.Model):
-
-    objects = IssueManager()
-
-    ISSUE_STATUS_OPEN = 'open'
-    ISSUE_STATUS_IN_PROGRESS = 'in_progress'
-    ISSUE_STATUS_CLOSED = 'closed'
-    ISSUE_STATUS_CHOICES = (
-        (ISSUE_STATUS_OPEN, _('open')),
-        (ISSUE_STATUS_IN_PROGRESS, _('in progress')),
-        (ISSUE_STATUS_CLOSED, _('closed'))
-    )
-
-    project = models.ForeignKey(
-        'Project', on_delete=models.CASCADE, related_name='issues',
-        verbose_name=_('Project'),
-        help_text=_('The project for this issue.')
-    )
-    task = models.ForeignKey(
-        Task, on_delete=models.CASCADE, related_name='issues',
-        verbose_name=_('Task'),
-        help_text=_('The task for this issue.')
-    )
-    status = models.CharField(
-        max_length=12, choices=ISSUE_STATUS_CHOICES, default=ISSUE_STATUS_OPEN,
-        verbose_name=_('Status'),
-        help_text=_('The status for this issue.')
-    )
-
-    class Meta:
-        ordering = ('project__title', )
-        verbose_name = _('Issue')
-        verbose_name_plural = _('Issues')
-
-    def __str__(self):
-        return '%s / %s / %s' % (self.project.title, self.task, self.status)
-
-    def get_absolute_url(self):
-        return reverse('project', kwargs={'pk': self.project.pk})
-
-    def resolve(self):
-        for condition in self.task.conditions.all():
-            if condition.resolve(self.project):
-                return True
-
-    @property
-    def dates(self):
-        values = self.project.values.filter(snapshot=None)
-
-        if self.task.start_attribute:
-            start_values = values.filter(attribute=self.task.start_attribute)
-        else:
-            start_values = []
-
-        if self.task.end_attribute:
-            end_values = values.filter(attribute=self.task.end_attribute)
-        else:
-            end_values = []
-
-        days_before = timedelta(self.task.days_before) if self.task.days_before else timedelta()
-        days_after = timedelta(self.task.days_after) if self.task.days_after else timedelta()
-
-        dates = []
-        for start_value, end_value in zip_longest(start_values, end_values):
-
-            if start_value and start_value.value and isinstance(start_value.value, date):
-                start_date = start_value.value
-            else:
-                start_date = None
-
-            if end_value and end_value.value and isinstance(end_value.value, date):
-                end_date = end_value.value
-            else:
-                end_date = None
-
-            if start_date and end_date:
-                dates.append((start_date - days_before, end_date + days_after))
-            elif start_date:
-                dates.append((start_date - days_before + days_after, ))
-            elif end_date:
-                dates.append((end_date - days_before + days_after, ))
-
-        return dates
 
 
 class Snapshot(Model):
